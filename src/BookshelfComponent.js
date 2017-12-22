3acfb35f064049a03bbfa9aef84bd70d853c336e
Jordan Myers <jordan.v.myers@gmail.com>
2017-12-22 11:41:34 -0600

    feature: add and complete BookshelfComponent
    The BookshelfComponent takes a name and a list of book objects and
    renders a bookshelf div with the list of BookComponents for each book.

---
+++ src/BookshelfComponent.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import * as BooksAPI from './BooksAPI'
+import BookComponent from './BookComponent'
+import './App.css'
+
+
+class BookshelfComponent extends React.Component {
+
+  render() {
+    const books = this.props.books
+    const name = this.props.name
+
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{name}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {books.map(book => (
+              <li>
+                <BookComponent
+                  title={book.title}
+                  authors={book.authors}
+                  imageLinks={book.imageLinks}
+                />
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+
+    )
+  }
+}
+export default BookshelfComponent
